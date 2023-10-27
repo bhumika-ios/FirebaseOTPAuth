7a2ea4b2babc0d6d47029172090bc63530fbee3a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-10-27 15:45:24 +0530

    Initial Commit

---
+++ FirebaseOTPAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseOTPAuth
+//
+//  Created by Bhumika Patel on 27/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseOTPAuth/FirebaseOTPAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseOTPAuthApp.swift
+//  FirebaseOTPAuth
+//
+//  Created by Bhumika Patel on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseOTPAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
