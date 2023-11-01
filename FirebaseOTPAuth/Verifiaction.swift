aff8c776ab0ca62f13586975adfd41f75fc40ad6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-01 17:13:23 +0530

    create verification view

---
+++ FirebaseOTPAuth/Verifiaction.swift
@@ -0,0 +1,18 @@
+//
+//  Verifiaction.swift
+//  FirebaseOTPAuth
+//
+//  Created by Bhumika Patel on 01/11/23.
+//
+
+import SwiftUI
+
+struct Verifiaction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Verifiaction()
+}
